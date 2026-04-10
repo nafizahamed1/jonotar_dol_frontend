ea09dcbeb68f495e2714a28d3eb1eddb57f1fcc7
Nafiz Ahamed Eshan <nafizahamed28@gmail.com>
2026-04-10 14:56:04 +0600

    update footer copyright to "© 2026 জনতার দল. All rights reserved."

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <footer className="bg-gray-900 text-white text-center p-4 mt-10">
-      <p>© 2026 Political Platform. All rights reserved.</p>
+      <p>© 2026 জনতার দল. All rights reserved.</p>
     </footer>
   );
 };
