116ebee01eb65f53b1a737b2626252619cdfaef6
Nafiz Ahamed Eshan <nafizahamed28@gmail.com>
2026-04-16 15:45:33 +0600

    leaders picture updated

--- src/pages/Leaders.tsx
+++ src/pages/Leaders.tsx
@@ -1,18 +1,35 @@
-import { useEffect, useState } from "react";
-import { getLeaders } from "../services/api";
-import type { Leader } from "../types/leader";
 import LeaderCard from "../components/LeaderCard";
+import leader1 from "../assets/leaders/leader1.jpg";
+import leader2 from "../assets/leaders/leader2.jpg";
+import leader3 from "../assets/leaders/leader3.jpg";
 
 const Leaders = () => {
-  const [leaders, setLeaders] = useState<Leader[]>([]);
-
-  useEffect(() => {
-    getLeaders().then(setLeaders);
-  }, []);
+  const leaders = [
+    {
+      _id: "1",
+      name: "বিগ্রেডিয়ার জেনারেল (অবঃ) মোঃ শামীম কামাল",
+      position: "চেয়ারম্যান, জনতার দল",
+      image: leader1,
+    },
+    {
+      _id: "2",
+      name: "ডেল এইচ খান",
+      position: "প্রধান সমন্বয়ক ও মুখপাত্র, জনতার দল",
+      image: leader2,
+    },
+    {
+      _id: "3",
+      name: "আজম খান",
+      position: "মহাসচিব, জনতার দল",
+      image: leader3,
+    },
+  ];
 
   return (
     <div className="container py-10">
-      <h1 className="text-3xl sm:text-4xl font-bold mb-6">Our Leaders</h1>
+      <h1 className="text-3xl sm:text-4xl font-bold mb-6">
+        Our Leaders
+      </h1>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
         {leaders.map((leader) => (
